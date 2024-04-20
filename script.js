732c55af17471763f98683cb458763fccba066d5
Takenori <takenori113@gmail.com>
2024-04-20 19:55:55 +0900

    といたよ

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
-const handlePost = (title, content) =>
-  fetch("https://jsonplaceholder.typicode.com/posts", {
+
+
+const title = document.querySelector("#submit").addEventListener("click", async (e) => {
+  e.preventDefault();
+  // title
+  const title = document.querySelector("#title").value;
+  const content = document.querySelector("#content").value;
+  // content
+  const res = await handlePost(title, content);
+  console.log(res);
+
+  document.querySelector("#result-id").textContent = `created POST ID is ${res.id}`
+  document.querySelector("#result-title").textContent = `created POST title is ${res.title}`
+  document.querySelector("#result-content").textContent = `created POST content is ${res.content}`
+
+});
+
+const handlePost = async (title, content) => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts", {
     method: "POST",
     body: JSON.stringify({
       title,
@@ -9,7 +26,9 @@ const handlePost = (title, content) =>
       "Content-type": "application/json; charset=UTF-8",
     },
   });
+  return res.json()
+}
 
-export const main = () => {};
+export const main = () => { };
 
 window.addEventListener("load", main);
