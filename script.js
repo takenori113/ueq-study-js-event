9d3aa47dc2395590cfb6c179248e28b87e0c63f1
Takenori <takenori113@gmail.com>
2024-04-22 12:21:30 +0900

    メイン関数荷処理を書き直しました。

--- script.js
+++ script.js
@@ -1,15 +1,6 @@
 
 
-document.querySelector("#submit").addEventListener("click", async (e) => {
-  e.preventDefault();
-  const title = document.querySelector("#title").value;
-  const content = document.querySelector("#content").value;
-  const res = await handlePost(title, content);
-  console.log(res);
-  document.querySelector("#result-id").textContent = `created POST ID is ${res.id}`
-  document.querySelector("#result-title").textContent = `created POST title is ${res.title}`
-  document.querySelector("#result-content").textContent = `created POST content is ${res.content}`
-});
+
 
 const handlePost = async (title, content) => {
   const res = await fetch("https://jsonplaceholder.typicode.com/posts", {
@@ -25,6 +16,15 @@ const handlePost = async (title, content) => {
   return res.json()
 }
 
-export const main = () => { };
+export const main = () => { document.querySelector("#submit").addEventListener("click", async (e) => {
+  e.preventDefault();
+  const title = document.querySelector("#title").value;
+  const content = document.querySelector("#content").value;
+  const res = await handlePost(title, content);
+  console.log(res);
+  document.querySelector("#result-id").textContent = `created post ID is ${res.id}`
+  document.querySelector("#result-title").textContent = `created post title is ${res.title}`
+  document.querySelector("#result-content").textContent = `created post content is ${res.content}`
+});};
 
 window.addEventListener("load", main);
